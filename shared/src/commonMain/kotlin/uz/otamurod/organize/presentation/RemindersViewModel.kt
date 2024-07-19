732310a0a900d3f66cf87b44c838dd30d5d08150
otamurod <safarovotamurod@gmail.com>
2024-07-19 09:20:25 +0500

    feat: create RemindersViewModel

---
+++ shared/src/commonMain/kotlin/uz/otamurod/organize/presentation/RemindersViewModel.kt
@@ -0,0 +1,30 @@
+package uz.otamurod.organize.presentation
+
+import uz.otamurod.organize.data.RemindersRepository
+import uz.otamurod.organize.domain.Reminder
+
+class RemindersViewModel : BaseViewModel() {
+    private val repository = RemindersRepository()
+    
+    private val reminders: List<Reminder>
+        get() = repository.reminders
+    
+    var onRemindersUpdated: ((List<Reminder>) -> Unit)? = null
+        set(value) {
+            field = value
+            onRemindersUpdated?.invoke(reminders)
+        }
+    
+    fun createReminder(title: String) {
+        val trimmed = title.trim()
+        if (trimmed.isNotEmpty()) {
+            repository.createReminder(title = trimmed)
+            onRemindersUpdated?.invoke(reminders)
+        }
+    }
+    
+    fun markReminder(id: String, isCompleted: Boolean) {
+        repository.markReminder(id = id, isCompleted = isCompleted)
+        onRemindersUpdated?.invoke(reminders)
+    }
+}
